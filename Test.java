d7553c347d5355a1cde63fbe2f1b6c67549a9189
terasi <hitokunn750@gmail.com>
2024-09-05 19:46:03 +0900

    instance ensyu2

--- Test.java
+++ Test.java
@@ -9,6 +9,27 @@ public class Test {
         System.out.println(taro.name);
         System.out.println(taro.age);
 
+        Person ziro=new Person();
+        ziro.name="木村次郎";
+        ziro.age=18;
+
+        System.out.println(ziro.name);
+        System.out.println(ziro.age);
+
+        Person hanako=new Person();
+        hanako.name="鈴木花子";
+        hanako.age=16;
+
+        System.out.println(hanako.name);
+        System.out.println(hanako.age);
+
+        Person hiroto=new Person();
+        hiroto.name="寺島滉人";
+        hiroto.age=27;
+
+        System.out.println(hiroto.name);
+        System.out.println(hiroto.age);
+
     }
 
 }
